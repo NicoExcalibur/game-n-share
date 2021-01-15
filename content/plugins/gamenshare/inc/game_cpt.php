d602ac1039622f77cba8749d0e8606d2702f32c3
NicoExcalibur <nico.zanz@gmail.com>
2021-01-15 17:29:44 +0100

    :construction:test solutions to clear cache & updated CPT to not show in gutenberg

--- content/plugins/gamenshare/inc/game_cpt.php
+++ content/plugins/gamenshare/inc/game_cpt.php
@@ -13,12 +13,13 @@ class Game_cpt
     public function __construct()
     {
       add_action('init', [$this, 'create_cpt']);
+      add_action('init', [$this, 'create_taxo']);
     }
 
      // declaration of the CPT
      public function create_cpt()
      {
-       $labels = [
+      $labels = [
         'name'               => 'Jeux',
         'singular_name'      => 'jeu',
         'menu_name'          => 'Jeux',
@@ -32,14 +33,14 @@ class Game_cpt
         'search_items'       => 'Rechercher un jeu',
         'not_found'          => 'Aucun jeu trouvé',
         'not_found_in_trash' => 'Aucun jeu trouvé dans la corbeille',
-       ];
+      ];
    
-       $args = [
+      $args = [
         'label' => 'Games',
         'labels' => $labels,
         'public' => true,
         'hierarchical' => false,
-
+        'show_in_nav_menus' => true,
         'menu_position' => 4,
         'menu_icon' => 'dashicons-carrot',
         'supports' => [
@@ -52,43 +53,48 @@ class Game_cpt
         ],
         'has_archive' => true,
         'rewrite' => [
-           'slug' => 'games',
-           'with_front' => true
+          'slug' => 'games',
+          'with_front' => true
         ]
-   
-       ];
+      ];
        // registration of the CPT
         register_post_type('game', $args);
     }
 
     public function create_taxo()
     {
-        $labels = [
-          'name'                       => 'Genres',
-          'singular_name'              => 'Genre',
-          'menu_name'                  => 'Genres',
-          'all_items'                  => 'Tout les genres',
-          'new_item_name'              => 'Nouveau genre',
-          'add_new_item'               => 'Ajouter un genre',
-          'update_item'                => 'Mettre à jour un genre',
-          'edit_item'                  => 'Editer un genre',
-          'view_item'                  => 'Voir tout les genres',
-          'separate_items_with_commas' => 'Séparer les genres avec une virgule',
-          'add_or_remove_items'        => 'Ajouter ou supprimer un genre',
-          'choose_from_most_used'      => 'Choisir parmis les genres les plus utilisées',
-          'popular_items'              => 'Genres populaires',
-          'search_items'               => 'Rechercher un genre',
-          'not_found'                  => 'Aucun genre trouvé',
-          'items_list'                 => 'Lister les genres',
-        ];
+      $labels = [
+        'name'                       => 'Genres',
+        'singular_name'              => 'Genre',
+        'menu_name'                  => 'Genres',
+        'all_items'                  => 'Tout les genres',
+        'new_item_name'              => 'Nouveau genre',
+        'add_new_item'               => 'Ajouter un genre',
+        'update_item'                => 'Mettre à jour un genre',
+        'edit_item'                  => 'Editer un genre',
+        'view_item'                  => 'Voir tout les genres',
+        'separate_items_with_commas' => 'Séparer les genres avec une virgule',
+        'add_or_remove_items'        => 'Ajouter ou supprimer un genre',
+        'choose_from_most_used'      => 'Choisir parmis les genres les plus utilisées',
+        'popular_items'              => 'Genres populaires',
+        'search_items'               => 'Rechercher un genre',
+        'not_found'                  => 'Aucun genre trouvé',
+        'items_list'                 => 'Lister les genres',
+      ];
 
-        $args = [
-          'labels'        => $labels,
-          'hierarchical'  => true,
-          'public'        => true
-        ];
+      $args = [
+        'labels'            => $labels,
+        'hierarchical'      => true,
+        'public'            => true,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'genre' ],
+        'rest_base'         => 'game_genre',
+        'show_in_nav_menus' => true
+      ];
 
-      register_taxonomy('genre', 'game', $args);
+      register_taxonomy('genre','game', $args);
 
     }
 
