ebb8579d6f4401da70dc78d3d8c1bd3faa9d3eef
NicoExcalibur <nico.zanz@gmail.com>
2021-01-19 17:36:41 +0100

    :hammer:add script for filters and filter button responsive

--- content/themes/gamenshare/src/js/app.js
+++ content/themes/gamenshare/src/js/app.js
@@ -6,11 +6,14 @@ const app = {
     btnClose: document.getElementById('btn-close'),
     blockSearch: document.getElementById('form-search'),
     formSearch: document.querySelector('.block-search__form'),
+    filters: document.querySelector('.dropdown-menu'),
+    filterButton: document.querySelector('.button-filter-mobile'),
 
     init: function () {
-        console.log('init');
+        //console.log('init');
         app.btnClose.addEventListener('click', app.closeSearch);
         app.formSearch.addEventListener('submit', app.handlecloseSearch);
+        window.addEventListener('resize', app.filtersResponsive);
     },
     closeSearch: function(){
         //console.log(app.blockSearch);
@@ -20,7 +23,22 @@ const app = {
         //console.log(evt);
         evt.preventDefault();
         app.blockSearch.classList.remove('show');
+    },
+
+    filtersResponsive: function(){
+        let lWidth = window.screen.width;
+        //console.log(lWidth);
+
+        if (lWidth >= 768) {
+            
+            app.filters.classList.remove('dropdown-menu');
+            app.filterButton.classList.add('d-none');
+        }else {
+            app.filters.classList.add('dropdown-menu');
+            app.filterButton.classList.remove('d-none');
+        }
     }
 
 }
 document.addEventListener('DOMContentLoaded', app.init);
+
