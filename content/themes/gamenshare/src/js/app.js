bf04b5f300f32cddc3f75d45aa7becc5953c814f
Nico <nico.zanz@gmail.com>
2021-02-07 17:12:25 +0100

    :bug:resolve conflicts and bugs

--- content/themes/gamenshare/src/js/app.js
+++ content/themes/gamenshare/src/js/app.js
@@ -1,11 +1,12 @@
 import '../scss/main.scss';
 
 import 'bootstrap/dist/js/bootstrap.bundle';
-
 import 'jquery-bar-rating/dist/jquery.barrating.min.js';
 import $ from 'jquery';
 
 window.jQuery = $;
+
+
 
 const app = {
     btnClose: document.getElementById('btn-close'),
@@ -14,98 +15,84 @@ const app = {
     filters: document.querySelector('.dropfilter'),
     filterButton: document.querySelector('.button-filter-mobile'),
     footerEl: document.querySelector('.footer'),
-    body: document.querySelector('body'),
+
     init: function () {
         //console.log('init');
-
         app.btnClose.addEventListener('click', app.closeSearch);
         app.formSearch.addEventListener('submit', app.handlecloseSearch);
-
         window.addEventListener('resize', app.filtersResponsive);
         window.addEventListener('load', app.filtersResponsive);
         window.addEventListener('scroll', app.checkIfScrolled);
         window.addEventListener('load', app.checkIfScrolled);
-
-
         app.handleAjaxFilterGames();
         app.handleStarRating();
 
     },
-    closeSearch: function () {
+    closeSearch: function(){
         //console.log(app.blockSearch);
         app.blockSearch.classList.remove('show');
     },
-    handlecloseSearch: function (evt) {
+    handlecloseSearch: function(evt) {
         //console.log(evt);
         evt.preventDefault();
         app.blockSearch.classList.remove('show');
     },
 
-    filtersResponsive: function () {
+    filtersResponsive: function(){
         let lWidth = window.screen.width;
         console.log(lWidth);
-        // console.log(app.filters);
-        //let myClass = app.filters.classList.contains('dropdown-menu');
-        //console.log(myClass);
-        let bodyClass = app.body.classList.contains('post-type-archive-game');
-        //console.log(bodyClass);
-
-        if (bodyClass) {
-            console.log('kikou');
-            
-            if (lWidth >= 768) {
-                //if (myClass) {
-                    app.filters.classList.remove('dropdown-menu');
-                //}
-                app.filterButton.classList.add('d-none');
-            } else {
-                app.filters.classList.add('dropdown-menu');
-                app.filterButton.classList.remove('d-none');
-            }
+        console.log(app.filters);
+        
+        if (lWidth >= 768) { 
+            app.filters.classList.remove('dropdown-menu');
+            app.filterButton.classList.add('d-none');
+        }else 
+        {
+            app.filters.classList.add('dropdown-menu');
+            app.filterButton.classList.remove('d-none');
         }
-
     },
 
-    checkIfScrolled: function () {
-        //console.log(app.footerEl.offsetHeight);
-        let bodyClass = app.body.classList.contains('post-type-archive-game');
-        if (bodyClass) {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            app.filterButton.style.bottom = app.footerEl.offsetHeight + "px";
-        } else if (document.body.offsetHeight - app.footerEl.offsetHeight <= window.innerHeight + window.scrollY) {
-            app.filterButton.style.bottom = (window.innerHeight + window.scrollY) - (document.body.offsetHeight - app.footerEl.offsetHeight) + 5 + "px";
-        } else {
+    checkIfScrolled: function(){
+        console.log(app.footerEl.offsetHeight);
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight )
+        {
+            app.filterButton.style.bottom = app.footerEl.offsetHeight  + "px";
+        }else if(document.body.offsetHeight - app.footerEl.offsetHeight <= window.innerHeight + window.scrollY)
+        {
+            app.filterButton.style.bottom =  (window.innerHeight + window.scrollY) - (document.body.offsetHeight - app.footerEl.offsetHeight) + 5 + "px";
+        }else 
+        {
             app.filterButton.style.bottom = "35px";
         }
-    }
     },
-    handleAjaxFilterGames: function () {
+    handleAjaxFilterGames: function() {
         //console.log('kikou j');
-
-        $('.form-check-input').change(function () {
+        
+            $('.form-check-input').change(function(){
             const filter = $('#filter');
             $.ajax({
-                url: filter.attr('action'),
-                data: filter.serialize(), // form data
-                type: filter.attr('method'), // POST
-
-                success: function (data) {
-
+                url:filter.attr('action'),
+                data:filter.serialize(), // form data
+                type:filter.attr('method'), // POST
+                
+                success:function(data){
+                  
                     $('#response').html(data); // insert data
                 }
             });
             return false;
         });
-        $('#filter').submit(function () {
+        $('#filter').submit(function(){
             const filter = $('#filter');
             $.ajax({
-                url: filter.attr('action'),
-                data: filter.serialize(), // form data
-                type: filter.attr('method'), // POST
-                beforeSend: function (xhr) {
+                url:filter.attr('action'),
+                data:filter.serialize(), // form data
+                type:filter.attr('method'), // POST
+                beforeSend:function(xhr){
                     filter.find('button').text('Processing...'); // changing the button label
                 },
-                success: function (data) {
+                success:function(data){
                     filter.find('button').text('Filtrez'); // changing the button label back
                     $('#response').html(data); // insert data
                 }
@@ -113,7 +100,6 @@ const app = {
             return false;
         });
     },
-
     handleStarRating: function () {
         $('.rating').barrating({
             theme: 'fontawesome-stars',
@@ -160,6 +146,7 @@ const app = {
             }
         });
     }
+    
 }
 
 document.addEventListener('DOMContentLoaded', app.init);
