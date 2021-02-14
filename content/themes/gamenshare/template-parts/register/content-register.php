f21acd7f04dba4ff1eafc292a92746f873f6eeca
Nico <nico.zanz@gmail.com>
2021-02-14 17:41:10 +0100

    :sparkles:add a user in DB

--- content/themes/gamenshare/template-parts/register/content-register.php
+++ content/themes/gamenshare/template-parts/register/content-register.php
@@ -1,8 +1,9 @@
-<form action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" method="post">
-    <input type="text" name="user_login" value="Username" id="user_login" class="input" />
-    <input type="text" name="user_email" value="E-Mail" id="user_email" class="input"  />
+<form class="mylogin" action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" method="post">
+    <input type="text" name="user_login" placeholder="Nom d'utilisateur" id="user_login" class="input" />
+    <input type="email" name="user_email" placeholder="E-mail" id="user_email" class="input"  />
+    <input type="password" name="user_pass" placeholder="Mot de passe" id="user_pass" class="input" />
     <?php do_action('register_form'); ?>
-    <input type="submit" value="Register" id="register" />
+    <input class="btn button-red" type="submit" value="Inscription" id="register" />
 </form>
 
 
