cc746b73e70133ca137dd90b049c3c0dfd244ab7
zephdev-92 <vzepharren@gmail.com>
2021-03-18 22:00:48 +0100

    clean code of page-jeux-video

--- content/themes/gamenshare/page-jeux-video.php
+++ content/themes/gamenshare/page-jeux-video.php
@@ -2,23 +2,7 @@
 
 get_header();
 
-?>
-
-<?php 
-while ( have_posts() ) :
-				the_post();
-
-
 get_template_part( 'template-parts/games/content-games', 'page' ); 
-
-endwhile;
-?>
-
-
-
-
-
-<?php 
 
 get_footer();
 
